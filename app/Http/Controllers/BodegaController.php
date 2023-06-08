8a67a9d686e082ebeaa1491466d794044858546a
JonathanLeal <jonathan.e.leal.m@gmail.com>
2023-06-08 13:39:23 -0600

    subiendo endpoint de listar

--- app/Http/Controllers/BodegaController.php
+++ app/Http/Controllers/BodegaController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Http;
+use App\Models\Bodega;
 use Illuminate\Http\Request;
 
 class BodegaController extends Controller
 {
-    //
+    public function obtenerBodegas() {
+        $bodegas = Bodega::all();
+        if (count($bodegas) == 0) {
+            return Http::respuesta(http::retNotFound, "No hay bodegas");
+        }
+        return http::respuesta(http::retOK, $bodegas);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BodegaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/bodegas', function () {
+    return view('Bodegas');
 });
+Route::get('bodega/list', [BodegaController::class, 'obtenerBodegas'])->name('get.bodegas');
