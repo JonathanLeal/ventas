7f12f88a0f4114ddb7ba0a55c79e15980f92dbe2
johanaUmanzor <johanita12rr@gmail.com>
2023-06-08 15:27:03 -0600

    actualizando

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -7,9 +7,19 @@ use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-   public function listaClientes(Request $request){
+    public function clienteSave(Request $request){
+        $cliente = new Cliente();
+            $cliente->id = $request->id;
+            $cliente->nombre_cli = $request->nombre_cli;
+            $cliente->save();
+
+            return $cliente;
+
+    }
+
+    public function listaClientes(Request $request){
      $cliente = Cliente::all($request);
-     
-     return ;
+
+     return $cliente;
    }
 }
